a2bc9ae4ce4e08ac7cbfeca3765ff573a75d2be6
arunmsharma <arunm2470@gmail.com>
2021-05-22 18:44:16 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const App = ()=>{
   return (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-    <small>Hello this is me</small>
-  </>
+  <div>
+    <h1>TIC TAC TOE</h1>
+  </div>
   )
 }
 
