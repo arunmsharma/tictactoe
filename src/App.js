8fc487273b2e120457570051eda20fc735966256
arunmsharma <arunm2470@gmail.com>
2021-05-24 14:30:09 +0530

    Dispalyed game history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React , {useState}from 'react';
 import Board from './components/Board';
 import { calculateWinner } from './helpers';
+import History from './components/History'
 
 import "./styles/root.scss"
 
@@ -64,13 +65,16 @@ const App = () => {
     setCurrentMove(prev => prev+1);
   }
 
-
+  const moveTo = (move) => {
+    setCurrentMove(move);
+  }
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board = {current.board} handleSquareClick= {handleSquareClick}/>
+      <History history={history} moveTo={moveTo}currentMove={currentMove}/>
     </div>
   );
 };
